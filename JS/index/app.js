1c27679047f0cd83f991bd8f4572da5d2218e2b3
slumbone <59657315+slumbone@users.noreply.github.com>
2021-07-29 21:48:23 +0900

    Update app.js

--- JS/index/app.js
+++ JS/index/app.js
@@ -16,10 +16,10 @@ class App {
     
     this.pixelRatio = (window.devicePixelRatio > 1) ? 2 : 1;
     
-    this.totalParticles = 15;
+    this.totalParticles = 3;
     this.particles = [];
-    this.maxRadius = 900;
-    this.minRadius = 400;
+    this.maxRadius = 90;
+    this.minRadius = 40;
     
     window.addEventListener('resize', this.resize.bind(this), false);
     this.resize();
